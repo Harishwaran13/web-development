23f00c83c763a766a91416adfebad46916dc1c74
Harish <68632685+Harishwaran13@users.noreply.github.com>
2021-11-10 09:10:57 +0530

    Add files via upload

---
+++ lab9/Lab9.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="Lab9.css">
+</head>
+<body>  
+<h2>Registration</h2>
+<form method="post" action="Lab9_data.php">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Mobile: <input type="text" name="website">
+  <br><br>
+   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="checkbox" name="Remember" value="Remember">Remember Me
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+
+
+</body>
+</html>
